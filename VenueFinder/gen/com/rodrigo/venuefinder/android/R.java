800901204d712a45cb9530b5d85bde22645f334d
RodrigoMirandaMarenco <rodrigo.kreloco@gmail.com>
2014-09-30 10:15:19 -0600

    Revert "PlaceHolder Changed"
    This reverts commit 5e5dc9a81f27103034d41e78bf6445f66c7b21f1.

--- VenueFinder/gen/com/rodrigo/venuefinder/android/R.java
+++ VenueFinder/gen/com/rodrigo/venuefinder/android/R.java
@@ -1068,7 +1068,6 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int gray=0x7f020059;
         public static final int ic_launcher=0x7f020057;
         public static final int transparent=0x7f020058;
     }
